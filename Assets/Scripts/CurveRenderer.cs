cfcb40b309db12dcf34d80dd80a75fd5ddfc650b
DESKTOP-CDUOM4H\dani <simionescudani07@gmail.com>
2020-08-06 18:52:23 +0300

    code;

---
+++ Assets/Scripts/CurveRenderer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CurveRenderer : MonoBehaviour
+{
+    public BezierCurve Path;
+    
+    //
+
+    private LineRenderer LineRenderer;
+    private int VertexCount = 200;
+
+    void Awake()
+    {
+        LineRenderer = gameObject.GetComponent<LineRenderer>();
+    }
+
+    void Start()
+    {
+        DrawCubicCurve();
+    }
+
+    void DrawCubicCurve(/*.anchorPoints*/)
+    {
+        var pathPoints = Path.GetAnchorPoints().ToList();
+
+        var allPositions = new List<Vector3>();
+
+        for (var curPointIndex = 0; curPointIndex < pathPoints.Count; curPointIndex++)
+        {
+            if ((curPointIndex + 1) >= pathPoints.Count)
+                break;
+
+            var curPoint = pathPoints[curPointIndex];
+            var nextPoint = pathPoints[curPointIndex + 1];
+
+            var point1 = curPoint.position;
+            var point2 = curPoint.globalHandle2;
+            var point3 = nextPoint.globalHandle1;
+            var point4 = nextPoint.position;
+
+            var positions = new Vector3[VertexCount];
+            for (int i = 1; i < VertexCount + 1; i++)
+            {
+                float t = i / (float)VertexCount;
+                positions[i - 1] = CaculateCubicBezierPoint(t, point1, point2, point3, point4);
+            }
+
+            allPositions.AddRange(positions);
+
+            LineRenderer.positionCount = allPositions.Count;
+            LineRenderer.SetPositions(allPositions.ToArray());
+        }
+    }
+
+    private Vector3 CaculateCubicBezierPoint(float t, Vector3 point1, Vector3 point2, Vector3 point3, Vector3 point4)
+    {
+        float u = 1 - t;
+        float tt = t * t;
+        float uu = u * u;
+        float uuu = uu * u;
+        float ttt = tt * t;
+
+        Vector3 point = uuu * point1;
+        point += 3 * uu * t * point2;
+        point += 3 * u * tt * point3;
+        point += ttt * point4;
+
+        return point;
+    }
+}
---
+++ Assets/Scripts/WeaponMovement.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponMovement : MonoBehaviour
+{
+    public Vector3 MousePosition;
+    public Animator Animator;
+    public Animator AnimatorExtension;
+    public Transform Hand;
+    //
+
+    private Transform _parent;
+    private int _tweenId;
+    private float _rotateDuration;
+    private IEnumerator _afterPartOfPathMoving;
+
+    private float _handFireTime;
+    private IEnumerator _afterFire;
+
+    private int _ltHandScaleId;
+    private readonly Vector3 _toHandScale = new Vector3(0.8f, 0.8f, 0.8f);
+
+    private bool _lookAtMouseClick;
+    private GameObject _go;
+
+    private float rotSpeed = 15f;
+
+    void Start()
+    {
+        _rotateDuration = 2f;
+        _handFireTime = 2f;
+
+        Hand.localScale = Vector3.zero;
+
+        Init();
+    }
+
+    public void Init()
+    {
+        //MoveAround();
+    }
+
+    void MoveAround()
+    {
+        _afterPartOfPathMoving = AfterPartOfPathMoving();
+        StartCoroutine(_afterPartOfPathMoving);
+        LTDescr _movePartOfPath = LeanTween.rotateAround(gameObject, Vector3.forward, 180f, _rotateDuration).setEase(LeanTweenType.linear);
+        _tweenId = _movePartOfPath.id;
+    }
+
+    void RotateToMousePosition()
+    {
+        _afterPartOfPathMoving = AfterPartOfPathMoving();
+        StartCoroutine(_afterPartOfPathMoving);
+        LTDescr _movePartOfPath = LeanTween.rotateAround(gameObject, Vector3.forward, 180f, _rotateDuration).setEase(LeanTweenType.linear);
+        _tweenId = _movePartOfPath.id;
+    }
+
+    IEnumerator AfterPartOfPathMoving()
+    {
+        yield return new WaitForSeconds(_rotateDuration);
+
+        StopRotating();
+        MoveAround();
+    }
+
+    private void StopRotating()
+    {
+        LeanTween.cancel(_tweenId);
+        StopCoroutine(_afterPartOfPathMoving);
+    }
+
+    void Update()
+    {
+        if (_lookAtMouseClick == false)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                FireHand();
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                CloseHand();
+
+            }
+        }
+
+        if (_lookAtMouseClick)
+        {
+            FaceEnemy();
+        }
+    }
+
+    private void FireHand()
+    {
+        MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (_go == null)
+        {
+            _go = Instantiate(Resources.Load("Prefabs/LookAt", typeof(GameObject)),
+                    Vector3.zero, Quaternion.identity
+                    ) as GameObject;
+            _go.transform.SetParent(transform.parent);
+        }
+        _lookAtMouseClick = true;
+        Animator.SetBool("SlingShot", true);
+        AnimatorExtension.SetBool("SlingShot", true);
+
+        _afterFire = AfterFire(_handFireTime);
+        StartCoroutine(_afterFire);
+
+        _ltHandScaleId  = LeanTween.scale(Hand.gameObject, _toHandScale, _handFireTime).setEase(LeanTweenType.easeOutBack).id;
+    }
+
+    private IEnumerator AfterFire(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        _afterFire = null;
+    }
+
+    private void CloseHand()
+    {
+        LeanTween.cancel(_ltHandScaleId);
+        if (_afterFire != null)
+            StopCoroutine(_afterFire);
+        _afterFire = null;
+
+        Hand.localScale = Vector3.zero;
+        _lookAtMouseClick = false;
+        Animator.SetBool("SlingShot", false);
+        AnimatorExtension.SetBool("SlingShot", false);
+    }
+
+    private void FaceEnemy()
+    {
+        // Move enemy inside localSpace to mousePosition
+        var lookAt = new Vector3(MousePosition.x, MousePosition.y, 0f);
+        transform.LookAt(lookAt);
+        _go.transform.position = new Vector3(MousePosition.x, MousePosition.y, 0f);
+    }
+}
